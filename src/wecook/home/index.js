90205bd1325652c74519646d151e0f23f322b6ee
Jennifer Zajac <zajac.je@northeastern.edu>
2022-12-16 12:25:41 -0500

    carousel

--- src/wecook/home/index.js
+++ src/wecook/home/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Carousel } from 'antd';
+import { Button } from "react-bootstrap";
+import { ReactComponent as Spatula } from './spatula.svg'
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h4>Home</h4>
+      <Carousel autoplay autoPlaySpeed={1000}>
+      <div className="slide slide1 d-flex align-items-center justify-content-center flex-column">
+      <h1 className="slideHeader">Welcome!</h1>
+      <h4 className="slideBody">Plan meals together with friends and family.</h4>
+      <Spatula className="spatula" />
+    </div>
+    <div className="slide slide2 d-flex align-items-center justify-content-center flex-column">
+    <h1 className="slideHeader">Thousands of recipes!</h1>
+    <h4 className="slideBody">Powered by Spoonacular API.</h4>
+    <h4 className="slideBody"><Button onClick={() => navigate("/explore")}>Start searching</Button></h4>
+    </div>
+    <div className="slide slide3 d-flex align-items-center justify-content-center flex-column">
+      <h1 className="slideHeader">Sign up today!</h1>
+      <h4 className="slideBody">Make a WeCook account for free.</h4>
+      <h4 className="slideBody"><Button onClick={() => navigate("/login")}>Register</Button></h4>
+    </div>
+  </Carousel>
     </>
   );
 };
